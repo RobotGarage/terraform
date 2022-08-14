8921daee256ff7342a7e917ef3259e6b71c7ca78
Robot Garage <jaxjason@gmail.com>
2022-08-13 22:32:20 -0400

    Update entry_table_loop.tf

--- google_data_catalog/using_entries/entry_table_loop.tf
+++ google_data_catalog/using_entries/entry_table_loop.tf
@@ -3,11 +3,14 @@ locals {
 }
 
 resource "google_data_catalog_entry" "table_a_entry" {
+  
+  user_specified_type = "Demo_type"
+  user_specified_system = "Demo_system"
 
   for_each = local.all_tables
 
   entry_group = google_data_catalog_entry_group.entry_group.id
   entry_id = each.key
 
-  schema = each.value.columns
+  schema = jsonencode(each.value)
 }
